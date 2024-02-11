ffae33f2928848d28170b7cc9816f36442f0fbff
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2024-02-12 01:50:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,9 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 curl -Lo ./globed-central-server-x64 https://github.com/dankmeme01/globed2/releases/download/v${VERSION}/globed-central-server-x64
 chmod +x ./globed-central-server-x64
 
-CMD ["/bin/bash", "/entrypoint.sh"]
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
