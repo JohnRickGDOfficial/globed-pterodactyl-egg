35692e2280f6ba700440e3ab9d1c1cc430cbab4d
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2024-02-12 00:37:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ sleep 2
 export HOME=/home/container
 cd /home/container
 
+uname -m
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 curl -Lo ./globed-central-server-x64 https://github.com/dankmeme01/globed2/releases/download/v${VERSION}/globed-central-server-x64
