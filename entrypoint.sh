ae54bc732b51c721afdbb76a3c6dda4f34d7164f
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2024-02-12 01:44:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,4 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 curl -Lo ./globed-central-server-x64 https://github.com/dankmeme01/globed2/releases/download/v${VERSION}/globed-central-server-x64
 chmod +x ./globed-central-server-x64
 
-# Run the Server
-./{{GLOBED_FILE}}
+CMD ["/bin/bash", "/entrypoint.sh"]
