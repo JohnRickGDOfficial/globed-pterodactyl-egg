cb98c51d6401dfe31d9037a3ece93cc49c2f1f66
DumbCaveSpider <56347227+DumbCaveSpider@users.noreply.github.com>
2024-02-12 00:22:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,13 @@
 sleep 2
 export HOME=/home/container
 cd /home/container
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+apt update
+apt install -y wget git make cmake g++ liblua5.3 libz-dev rapidjson-dev libcurl4-openssl-dev libboost-all-dev libssl-dev
 
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+wget https://github.com/dankmeme01/globed2/releases/download/v${VERSION}/globed-central-server-x64
+chmod +x *
 
 # Run the Server
 bash ./globed-central-server-x64
